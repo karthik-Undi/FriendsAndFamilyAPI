d6fda1796ad144fc09aa08b2b7b3f88d647f147d
Karthik <karthik.venkata@kanini.com>
2021-03-31 12:42:29 +0530

    Add project files.

---
+++ FriendsAndFamilyAPI/Repositories/FaFRepos.cs
@@ -0,0 +1,53 @@
+﻿using FriendsAndFamilyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendsAndFamilyAPI.Repositories
+{
+    public class FaFRepos: IFaFRepos
+    {
+        private readonly CommunityGateDatabaseContext _context;
+
+        public FaFRepos()
+        {
+
+        }
+
+        public FaFRepos(CommunityGateDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<FriendsAndFamily> GetAllFriendsAndFamily()
+        {
+            return _context.FriendsAndFamily.ToList();
+        }
+
+        public IEnumerable<FriendsAndFamily> GetFriendsAndFamilybyResidentId(int id)
+        {
+            return _context.FriendsAndFamily.Where(f=>f.ResidentId==id).ToList();
+        }
+
+        public async Task<FriendsAndFamily> PostFriendsAndFamily(FriendsAndFamily item)
+        {
+            FriendsAndFamily faf = null;
+            if (item == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                faf = new FriendsAndFamily() {
+                    FaFname = item.FaFname,
+                    Relation = item.Relation,
+                    ResidentId = item.ResidentId
+                };
+                await _context.FriendsAndFamily.AddAsync(faf);
+                await _context.SaveChangesAsync();
+            }
+            return faf;
+        }
+    }
+}
---
+++ FriendsAndFamilyAPI/Repositories/IFaFRepos.cs
@@ -0,0 +1,17 @@
+﻿using FriendsAndFamilyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendsAndFamilyAPI.Repositories
+{
+    public interface IFaFRepos
+    {
+        public IEnumerable<FriendsAndFamily> GetAllFriendsAndFamily();
+        public IEnumerable<FriendsAndFamily> GetFriendsAndFamilybyResidentId(int id);
+        public Task<FriendsAndFamily> PostFriendsAndFamily(FriendsAndFamily item);
+
+
+    }
+}
